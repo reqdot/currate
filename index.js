e299c0f913792f43270576aac76c35603c620884
replt <repl8297@gmail.com>
2018-02-12 12:54:17 +0900

    to divide passport authentication process into passport.js and authRoutes.js

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
 const express = require('express');
 const app = express();
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys.js');
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    accessToken => {
-      console.log(accessToken);
-    }
-  )
-);
-
-app.get(
-  '/auth/google',
-  passport.authenticate('google', { scope: ['profile', 'email'] })
-);
+require('./services/passport');
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
