e7ef3ee15a74bd03209509cc9546df0e21b323d9
replt <repl8297@gmail.com>
2018-02-12 13:39:30 +0900

    using cookie-session to handle the cookie(including logout)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
 
@@ -9,6 +10,16 @@ mongoose.connect(keys.mongoURI);
 require('./models/User');
 require('./services/passport');
 require('./routes/authRoutes')(app);
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
