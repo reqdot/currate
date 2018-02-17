10a85e84649a7afbd4870d045029b071125a1a88
replt <repl8297@gmail.com>
2018-02-17 11:29:18 +0900

    POST /api/users, set up the token config

--- models/User.js
+++ models/User.js
@@ -1,8 +1,60 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
 var userSchema = new Schema({
-  googleId: String
+  googleId: String,
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: value => {
+        return validator.isEmail(value);
+      },
+      message: `{VALUE} is not a valid email`
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  tokens: [
+    {
+      access: {
+        type: String,
+        required: true
+      },
+      token: {
+        type: String,
+        required: true
+      }
+    }
+  ]
 });
+userSchema.methods.toJSON = function() {
+  const user = this;
+  const userObject = user.toObject();
+  return _.pick(userObject, ['_id', 'email']);
+};
+
+userSchema.methods.generateAuthToken = function() {
+  const user = this;
+  const access = 'auth';
+  const token = jwt
+    .sign({ _id: user._id.toHexString(), access }, 'abc123')
+    .toString();
+
+  user.tokens = user.tokens.concat([{ access, token }]);
+
+  return user.save().then(() => {
+    return token;
+  });
+};
 
 mongoose.model('users', userSchema);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,4 +1,9 @@
+const _ = require('lodash');
+const mongoose = require('mongoose');
+const requireLogin = require('../middlewares/requireLogin');
 const passport = require('passport');
+
+const User = mongoose.model('users');
 
 module.exports = app => {
   app.get(
@@ -13,6 +18,23 @@ module.exports = app => {
       res.redirect('/bulletins');
     }
   );
+
+  app.post('/api/users', (req, res) => {
+    const body = _.pick(req.body, ['email', 'password']);
+    const user = new User(body);
+
+    user
+      .save()
+      .then(() => {
+        return user.generateAuthToken();
+      })
+      .then(token => {
+        res.header('x-auth', token).send(user);
+      })
+      .catch(e => {
+        res.status(400).send(e);
+      });
+  });
 
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
