46b71d3eb4c407d826038fc78646ec5da8b2ad91
replt <repl8297@gmail.com>
2018-02-17 16:14:47 +0900

    hashing password before saving

--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var userSchema = new Schema({
   googleId: String,
@@ -77,5 +78,20 @@ userSchema.statics.findByToken = function(token) {
     'tokens.access': 'auth'
   });
 };
+
+userSchema.pre('save', function(next) {
+  const user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 
 mongoose.model('users', userSchema);
--- routes/hashing.js
+++ routes/hashing.js
@@ -1,15 +1,30 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+const password = '123abc!';
 
-const token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-const decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+var hashedPassword =
+  '$2a$10$eG7NQsJm6NEEpkYTSfRUROnWqs3wGLAYZfSqFAWHm8Euh2pKOWduC';
+
+bcrypt.compare('123!', hashedPassword, (err, res) => {
+  console.log(res);
+});
+// var data = {
+//   id: 10
+// };
+//
+// const token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// const decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 // const message = 'I am user number 3';
 // const hash = SHA256(message).toString();
 //
